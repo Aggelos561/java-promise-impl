8383a13d8544b6b5be33abf041043437a570693d
Aggelos561 <angelos561@hotmail.com>
2024-11-30 21:28:46 +0200

    Better threading synchronization implementation.

--- java-promise/lib/src/main/java/gr/uoa/di/promise/old.java
+++ java-promise/lib/src/main/java/gr/uoa/di/promise/old.java
@@ -6,8 +6,10 @@ import java.util.function.Function;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
+import java.util.Queue;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
+
 
 /*
  * > "What I cannot create, I do not understand"
@@ -38,7 +40,7 @@ import java.util.concurrent.atomic.AtomicInteger;
  * A helpful guide to help you understand Promises is available here:
  * https://javascript.info/async
  */
-public class Promise<V> {
+public class old<V> {
 
     public static enum Status {
         PENDING,
@@ -46,96 +48,184 @@ public class Promise<V> {
         REJECTED
     }
 
-    private volatile Status promiseStatus;
-    private volatile ValueOrError<V> returnValue;
+    class PromiseData {
 
-    public Promise(PromiseExecutor<V> executor) {
-        promiseStatus = Status.PENDING;
-        makePromise(executor);
+        private Status promiseStatus;
+        private ValueOrError<V> returnValue;
+        private final Queue<Function<V, ?>> fulfilledQueue;
+        private final Queue<Consumer<Throwable>> rejectedQueue;
+    
+        public PromiseData() {
+            promiseStatus = Status.PENDING;
+            returnValue = null;
+            fulfilledQueue = new LinkedList<>();
+            rejectedQueue = new LinkedList<>();
+        }
+    
+        public synchronized Status getPromiseStatus() {
+            return promiseStatus;
+        }
+    
+        public synchronized void setPromiseStatus(Status status) {
+            this.promiseStatus = status;
+        }
+    
+        public synchronized ValueOrError<V> getReturnValue() {
+            return returnValue;
+        }
+    
+        public synchronized void setReturnValue(ValueOrError<V> returnValue) {
+            this.returnValue = returnValue;
+        }
+    
+        public synchronized void addToFulfQueue(Function<V, ?> callback) {
+            fulfilledQueue.add(callback);
+        }
+    
+        public synchronized Function<V, ?> getNextResolve() {
+            return fulfilledQueue.poll();
+        }
+    
+        public synchronized boolean hasNextResolve() {
+            return !fulfilledQueue.isEmpty();
+        }
+    
+        public synchronized void addToRejQueue(Consumer<Throwable> callback) {
+            rejectedQueue.add(callback);
+        }
+    
+        public synchronized Consumer<Throwable> getNextReject() {
+            return rejectedQueue.poll();
+        }
+    
+        public synchronized boolean hasNextReject() {
+            return !rejectedQueue.isEmpty();
+        }
     }
 
-    private void makePromise(PromiseExecutor<V> executor) {
+    public PromiseData promData;
+
+    public old(PromiseExecutor<V> executor) {
+        this.promData = new PromiseData();
+        startPromise(executor);
+    }
+
+    private void startPromise(PromiseExecutor<V> executor) {
         new Thread(() -> {
-            executor.execute(this::resolvePromise, this::rejectPromise);
+            executor.execute(this::onResolve, this::onReject);
         }).start();
     }
 
-    private synchronized void resolvePromise(V value) {
-        if (promiseStatus != Status.PENDING)
+    private void onResolve(V value) {
+        if (promData.getPromiseStatus() != Status.PENDING) 
             return;
 
-        promiseStatus = Status.FULFILLED;
-        returnValue = ValueOrError.Value.of(value);
+        promData.setPromiseStatus(Status.FULFILLED);
+        promData.setReturnValue(ValueOrError.Value.of(value));
 
-        notifyAll();
-    }
-
-    private synchronized void rejectPromise(Throwable error) {
-        if (promiseStatus != Status.PENDING)
-            return;
-
-        promiseStatus = Status.REJECTED;
-        returnValue = ValueOrError.Error.of(error);
-
-        notifyAll();
-    }
-
-    private synchronized void waitPendingPromise() {
-        while (promiseStatus == Status.PENDING) {
-            try {
-                wait();
-            }
-            catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+        while (promData.hasNextResolve()) {
+            Function<V, ?> callback = promData.getNextResolve();
+            callback.apply(value);
         }
     }
 
-    private <T> void settlePromise(Function<V, T> onResolve, Consumer<Throwable> onReject, 
-                                Consumer<T> resolve, Consumer<Throwable> reject)
-    {
-        try {
-            if (promiseStatus == Status.FULFILLED) {
-                V value = returnValue.value();
-                T result = onResolve.apply(value);
-                resolve.accept(result);
-            }
-            else if (promiseStatus == Status.REJECTED) {
-                Throwable error = returnValue.error();
-                onReject.accept(error);
-                reject.accept(error);
-            }
-        }
-        catch (Exception e) {
-            reject.accept(e);
+    private void onReject(Throwable error) {
+        if (promData.getPromiseStatus() != Status.PENDING) 
+            return;
+
+        promData.setPromiseStatus(Status.REJECTED);
+        promData.setReturnValue(ValueOrError.Error.of(error));
+
+        while (promData.hasNextReject()) {
+            Consumer<Throwable> callback = promData.getNextReject();
+            callback.accept(error);
         }
     }
 
     public <T> Promise<T> then(Function<V, T> onResolve, Consumer<Throwable> onReject) {
         return new Promise<>((resolve, reject) -> {
-            waitPendingPromise();
-            settlePromise(onResolve, onReject, resolve, reject);
+            if (promData.getPromiseStatus() == Status.PENDING) {
+                
+                promData.addToFulfQueue(value -> {
+                    try {
+                        T result = onResolve.apply(value);
+                        resolve.accept(result);
+                        return result;
+                    } 
+                    catch (Exception e) {
+                        reject.accept(e);
+                        return null;
+                    }
+                });
+
+                promData.addToRejQueue(error -> {
+                    try {
+                        onReject.accept(error); 
+                        reject.accept(error);
+                    } 
+                    catch (Exception e) {
+                        reject.accept(e);
+                    }
+                });
+            } 
+            else if (promData.getPromiseStatus() == Status.FULFILLED) {
+                try {
+                    T result = onResolve.apply(promData.getReturnValue().value());
+                    resolve.accept(result);
+                } 
+                catch (Exception e) {
+                    reject.accept(e);
+                }
+            }
+            else if (promData.getPromiseStatus() == Status.REJECTED) {
+                try {
+                    onReject.accept(promData.getReturnValue().error());
+                    reject.accept(promData.getReturnValue().error());
+                } 
+                catch (Exception e) {
+                    reject.accept(e);
+                }
+            }
         });
     }
 
     public <T> Promise<T> then(Function<V, T> onResolve) {
-        return then(onResolve, (error) -> {});
+        return then(onResolve, null);
     }
 
     // catch is a reserved word in Java.
     public Promise<?> catchError(Consumer<Throwable> onReject) {
-        return then((value) -> {return value;}, onReject);
+        return then(null, onReject);
     }
 
     public Promise<V> andFinally(Consumer<ValueOrError<V>> onSettle) {
-        return then
-            ((value) -> {
-                onSettle.accept(ValueOrError.Value.of(value));
-                return value;
-            },
-            (error) -> {
-                onSettle.accept(ValueOrError.Error.of(error));
-            });
+        return new Promise<>((resolve, reject) -> {
+            if (promData.getPromiseStatus() == Status.PENDING) {
+
+                promData.addToFulfQueue(value -> {
+                    onSettle.accept(ValueOrError.Value.of(value));
+                    resolve.accept(value);
+                    return null;
+                });
+
+                promData.addToRejQueue(error -> {
+                    onSettle.accept(ValueOrError.Error.of(error));
+                    reject.accept(error);
+                });
+            } 
+            else {
+                if (promData.getPromiseStatus() == Status.FULFILLED) {
+                    V value = promData.getReturnValue().value();
+                    onSettle.accept(ValueOrError.Value.of(value));
+                    resolve.accept(value);
+                } 
+                else if (promData.getPromiseStatus() == Status.REJECTED) {
+                    Throwable error = promData.getReturnValue().error();
+                    onSettle.accept(ValueOrError.Error.of(error));
+                    reject.accept(error);
+                }
+            }
+        });
     }
 
     public static <T> Promise<T> resolve(T value) {
@@ -153,12 +243,12 @@ public class Promise<V> {
     public static Promise<ValueOrError<?>> race(List<Promise<?>> promises) {
         return new Promise<>((resolve, reject) -> {
             for (Promise<?> promise : promises) {
-                promise.then((value) -> {
-                    resolve.accept(ValueOrError.Value.of(value));
-                    return value;
+                promise.then((result) -> {
+                    resolve.accept(ValueOrError.Value.of(result));
+                    return result;
                 },
-                (error) -> {
-                    reject.accept(error);
+                (result) -> {
+                    reject.accept(result);
                 });
             }
         });
@@ -166,31 +256,35 @@ public class Promise<V> {
 
     public static Promise<?> any(List<Promise<?>> promises) {
         if (promises.isEmpty()) {
+            // Immediately reject if the input list is empty
             return new Promise<>((resolve, reject) -> {
-                reject.accept(new Throwable("Empty List Of Promises"));
+                reject.accept(new Throwable("No promises provided"));
             });
         }
     
-        AtomicInteger remPromises = new AtomicInteger(promises.size());
+        AtomicBoolean resolved = new AtomicBoolean(false);
+        AtomicInteger remaining = new AtomicInteger(promises.size());
+    
         return new Promise<>((resolve, reject) -> {
             for (Promise<?> promise : promises) {
                 promise.then(
-                    (value) -> {
-                        resolve.accept(value);
-                        return value;
+                    (result) -> {
+                        // Resolve as soon as one promise fulfills
+                        if (resolved.compareAndSet(false, true)) {
+                            resolve.accept(result);
+                        }
+                        return result;
                     },
                     (error) -> {
-                        int promisesLeft = remPromises.decrementAndGet();
-                        if (promisesLeft == 0) {
-                            reject.accept(new Throwable("All Promises Got Rejected"));
-                            return;
+                        // Check if all promises are rejected
+                        if (remaining.decrementAndGet() == 0 && resolved.compareAndSet(false, true)) {
+                            reject.accept(new Throwable("All promises were rejected"));
                         }
                     }
                 );
             }
         });
     }
-
     public static Promise<List<?>> all(List<Promise<?>> promises) {
     if (promises.isEmpty()) {
         // Return immediately with an empty list if no promises are provided
@@ -203,7 +297,6 @@ public class Promise<V> {
 
     return new Promise<>((resolve, reject) -> {
         for (int i = 0; i < promises.size(); i++) {
-            
             final int index = i; // Capture index for ordered results
             Promise<?> promise = promises.get(i);
 
